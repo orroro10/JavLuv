953b5ae24121097c97f7e0429592eebc47cea1bd
Al <orroro10@proton.me>
2025-12-14 21:00:15 -0500

    Modified build.bat and src/JavLuv.sln to skip Test and Setup projects during build process.

--- build.bat
+++ build.bat
@@ -23,22 +23,22 @@ MSBuild JavLuv.sln -t:build -restore -p:RestorePackagesConfig=true /p:Configurat
   EXIT /B 1
 )
 
-cd ../setup/
-MSBuild Setup.sln /p:Configuration=Release && (
-  echo JavLuv Setup build succeeded
-) || (
-  echo JavLuv Setup build failed
-  EXIT /B 1
-)
+@REM cd ../setup/
+@REM MSBuild Setup.sln /p:Configuration=Release && (
+@REM   echo JavLuv Setup build succeeded
+@REM ) || (
+@REM   echo JavLuv Setup build failed
+@REM   EXIT /B 1
+@REM )
 
 cd ..
 
-copy /y "setup\bin\Release\Setup-JavLuv.msi" "build\Setup-JavLuv.msi" && (
-  echo Copied setup file to build folder
-) || (
-  echo JavLuv Setup copy failed
-  EXIT /B 1
-)
+@REM copy /y "setup\bin\Release\Setup-JavLuv.msi" "build\Setup-JavLuv.msi" && (
+@REM   echo Copied setup file to build folder
+@REM ) || (
+@REM   echo JavLuv Setup copy failed
+@REM   EXIT /B 1
+@REM )
 
 copy /y "src\JavLuv\bin\x64\Release\*.dll" "build\JavLuv\"
 copy /y "src\JavLuv\bin\x64\Release\*.exe" "build\JavLuv\"
@@ -46,25 +46,25 @@ copy /y "src\JavLuv\bin\x64\Release\JavLuv.exe.config" "build\JavLuv\"
 copy /y "src\JavLuv\bin\x64\Release\Core14.profile.xml" "build\JavLuv\"
 copy /y "src\JavLuv\bin\x64\Release\runtimes\win-x64\native\WebView2Loader.dll" "build\JavLuv\WebView2Loader.dll"
 
-cd build
+@REM cd build
 
-@for /f "tokens=* usebackq" %%f in (`git tag --sort=committerdate`) do @set "tag=%%f"
+@REM @for /f "tokens=* usebackq" %%f in (`git tag --sort=committerdate`) do @set "tag=%%f"
 
-set JavLuv=JavLuv-%tag%
+@REM set JavLuv=JavLuv-%tag%
 
-rename JavLuv %JavLuv%
-rename "Setup-JavLuv.msi" "Setup-%JavLuv%.msi"
+@REM rename JavLuv %JavLuv%
+@REM rename "Setup-JavLuv.msi" "Setup-%JavLuv%.msi"
 
-tar -a -c -f %JavLuv%.zip %JavLuv%  && (
-  echo Copied setup file to build folder
-) || (
-  echo JavLuv tar archive failed
-  EXIT /B 1
-)
+@REM tar -a -c -f %JavLuv%.zip %JavLuv%  && (
+@REM   echo Copied setup file to build folder
+@REM ) || (
+@REM   echo JavLuv tar archive failed
+@REM   EXIT /B 1
+@REM )
 
-del /s /q %JavLuv%
-rmdir /s /q %JavLuv%
+@REM del /s /q %JavLuv%
+@REM rmdir /s /q %JavLuv%
 
-cd ..
+@REM cd ..
 
 EXIT /B 0
